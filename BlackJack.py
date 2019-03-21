c2d416798ecbaae6b1e46099de56d3fc440061a5
AlphaNiner1415 <manchesternd@gmail.com>
2019-03-21 09:49:55 +0700

    Starting

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,18 @@
 
-print ("\U0001F0A")
+print('\u2660') #spades
+print('\u2661') #hearts
+print('\u2663') #clubs
+print('\u2666')
+
+#print('\u1f0a0')
 class Card:
-  def __init__ (self, number, suit, symbol):
+    def __init__ (self, number, suit, symbol):
       self.number = number
       self.suit = suit
       self.symbol = symbol
+class HeartCard(Card):
+    def __init__(self, number, suit,symbol):
+        self.symbol = '\u2661'
+        self.number = number
+        self.suit = "Hearts"
+aceHearts = HeartCard("A")
