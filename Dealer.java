9d64c0edc68d960d75baf1218839fa433837a195
AlphaNiner1415 <manchesternd@gmail.com>
2019-08-09 22:16:08 +0700

    Modify dealer

--- Dealer.java
+++ Dealer.java
@@ -1,17 +1,20 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Random;
-public class Dealer extends Player{
-    public Dealer(){
+
+public class Dealer extends Player {
+    public boolean play = false;
+
+    public Dealer() {
         super();
     }
-    public Dealer(String name, Card[] hand){
-        super(name,hand);
+
+    public Dealer(String name, Card[] hand) {
+        super(name, hand);
     }
-    public void printShowHand(){
-        super.printHand();
-    }
-    public void printHand(){
-        System.out.println("This is the dealer's hand: ");
+
+    public String printHand() {
+        String introString = "This is the dealer's hand: ";
         StringBuilder sb = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
         StringBuilder sb3 = new StringBuilder();
@@ -27,38 +30,80 @@ public class Dealer extends Player{
         }
         int n = (getNonNullHandLength() + 4) * 3;
         lineOne = stringMultiply("_", n);
-        lineTwo += sb.toString() + stringMultiply(" ", 12 - 2 * getNonNullHandLength()) + "|"; 
+        lineTwo += sb.toString() + stringMultiply(" ", 12 - 2 * getNonNullHandLength()) + "|";
         lineThree += sb2.toString() + stringMultiply(" ", 12 - 2 * getNonNullHandLength()) + "|";
         lineFour += sb3.toString() + stringMultiply(" ", 12 - 2 * getNonNullHandLength()) + "|";
         lineFive = stringMultiply("_", n);
-        System.out.println(lineOne);
-        System.out.println(lineTwo);
-        System.out.println(lineThree);
-        System.out.println(lineFour);
-        System.out.println(lineFive);
+        String totalString = introString + "\n" + lineOne + "\n" + lineTwo + "\n" + lineThree + "\n" + lineFour + "\n"
+                + lineFive + "\nTotal: " + this.total;
+        return totalString;
     }
-    public void decisionMaker(ArrayList<Card> deck){
-        while(this.total < 21 && this.getNonNullHandLength() < 5){
-            Random rand = new Random();
-            computeTotal();
-            if (this.total >= 18){
-                printShowHand();
-                break;
+
+    public String printShowHand() {
+        String returnString = super.printHand();
+        returnString = returnString.substring(18);
+        return returnString;
+    }
+
+    public void decisionMaker(ArrayList<Card> deck) {
+        this.computeTotal();
+        Random rand = new Random();
+        while (this.computeTotal() < 21 && this.getNonNullHandLength() < 5) {
+
+            if (getNonNullHandLength() == 2) {
+                if (this.computeTotal() <= 16) {
+                    this.draw(deck);
+                } else if (this.computeTotal() >= 17) {
+                    break;
+                }
+            } else if (getNonNullHandLength() == 3) {
+                if (this.computeTotal() >= 17) {
+                    break;
+                } else {
+
+                    Double r = rand.nextDouble();
+                    r = Double.parseDouble(new DecimalFormat("#.00").format(r));
+                    if (r <= 0.67) {
+                        this.draw(deck);
+                    } else {
+                        break;
+                    }
+                }
+            } else if (getNonNullHandLength() == 4) {
+                if (this.computeTotal() >= 17) {
+                    break;
+                } else {
+                    Double r = Double.parseDouble(new DecimalFormat("#.00").format(rand.nextDouble()));
+                    if (r <= 0.22) {
+                        this.draw(deck);
+                    } else {
+                        break;
+                    }
+                }
             }
-            if (getNonNullHandLength() == 2 && this.total <= 17) {
-                draw(deck);
-            } else if(getNonNullHandLength()==2 && this.total >= 18){
-                printShowHand();
+
+        }
+        System.out.println("Exiting Decision Maker!");
+
+    }
+
+    public void draw(ArrayList<Card> deck) {
+        Random rand = new Random();
+        int seed = rand.nextInt();
+        rand.setSeed(seed);
+        int cardDraw = rand.nextInt(deck.size());
+        Card drawnCard = deck.get(cardDraw);
+        deck.remove(cardDraw);
+        for (int i = 0; i < hand.length; i++) {
+
+            if (hand[i] == null) {
+                hand[i] = drawnCard;
+                System.out.println(hand[i]);
+                System.out.println(this.getName() + " drew a Card!\n");
+
                 break;
-            }
-            printHand();
-            
-            System.out.println(this.total);
-            if (getNonNullHandLength() == 3 && this.total <= 15 && rand.nextInt(10) <= 6) {
-                draw(deck);
             }
         }
-        System.out.println("Exiting decision Maker!");
-        
     }
+
 }
